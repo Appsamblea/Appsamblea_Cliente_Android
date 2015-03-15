c5ec220cef61586c1a6259e2563a7991f2a9a65f
carltorres <togacharls@gmail.com>
2015-03-15 12:52:56 +0100

    GrupoListView terminado! closes #59

--- Appsamblea/app/src/main/java/cliente/appsamblea/ListAdapters/GrupoListAdapter.java
+++ Appsamblea/app/src/main/java/cliente/appsamblea/ListAdapters/GrupoListAdapter.java
@@ -1,36 +1,38 @@
 package cliente.appsamblea.ListAdapters;
 
 import cliente.appsamblea.R;
-import cliente.appsamblea.ItemsManagers.itemAsamblea;
+import cliente.appsamblea.ItemsManagers.itemGrupo;
 import java.util.ArrayList;
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.ImageView;
 
 /**
- * Created by carlillos on 29/01/2015.
+ * Created by carlos on 29/01/2015.
  */
-public class GrupoListAdapter {
+public class GrupoListAdapter implements ListAdapter {
     private Context context;
-    protected ArrayList<String> elementos;
-    protected ArrayList<itemAsamblea> items;
+    protected ArrayList<String> referencias;
+    protected ArrayList<itemGrupo> grupos;
     private int layout;
 
-    /*public GrupoListAdapter(Context c, int l, ArrayList<String> e, ArrayList<EjercicioRutina> er){
+    public GrupoListAdapter(Context c, ArrayList<String> e, ArrayList<itemGrupo> litems){
         context = c;
-        elementos = e;
-        layout = l;
+        referencias = e;
+        layout = R.layout.item_grupo;
         if(e == null){
-            ejerciciosrutina = er;
-            elementos = null;
+            grupos = litems;
+            referencias = null;
         }
         else{
-            elementos = e;
-            ejerciciosrutina = null;
+            referencias = e;
+            grupos= null;
         }
     }
 
@@ -40,9 +42,19 @@ public class GrupoListAdapter {
     }
 
     @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
     public int getCount() {
-        if(elementos!=null )return elementos.size();
-        else return ejerciciosrutina.size();
+        if(referencias!=null )return referencias.size();
+        else return grupos.size();
     }
 
     @Override
@@ -56,16 +68,38 @@ public class GrupoListAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = context.getLayoutInflater();
-        View row;
-        row = inflater.inflate(layout, parent, false);
-        /*Asginar los atributos a los elementos del layot a partir de su ID
-        Ejemplo:
-            TextView texto;
-        	texto = (TextView) row.findViewById(R.id.nombreElemento);
+    public View getView(int posicion, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(layout, parent, false);
 
+        grupos.get(posicion).setImagen((ImageView)row.findViewById(R.id.imagenGrupoItem));
+        grupos.get(posicion).setNombre((TextView) row.findViewById(R.id.nombreGrupoItem));
 
         return (row);
-    }*/
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
 }
