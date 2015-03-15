0c4b83aa6e21769e0426172e74a7cc4909c325bc
Daniel Díaz Salas <potrayarrick@gmail.com>
2015-03-15 13:28:03 +0100

    Cambiado el build de gradle para que se pueda hacer debug

--- Appsamblea/app/src/main/java/cliente/appsamblea/Activities/NavigationDrawerFragment.java
+++ Appsamblea/app/src/main/java/cliente/appsamblea/Activities/NavigationDrawerFragment.java
@@ -22,6 +22,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import cliente.appsamblea.R;
+
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
  * See the <a href="https://developer.android.com/design/patterns/navigation-drawer.html#Interaction">
